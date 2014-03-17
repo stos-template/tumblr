50c51d2500ee3361d860435dfc422fe2131c281b
Shiho Saito <shiho.saito@gree.net>
2014-03-17 18:45:04 +0900

    修正

--- config.rb
+++ config.rb
@@ -9,6 +9,7 @@ images_dir = "./img"
 javascripts_dir = "./js"
 line_comments = false
 output_style = (environment == :production) ? :compressed : :compressed
+cache = false
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
